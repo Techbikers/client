0c44ce344b771cf314a2da30c0c620eddd2bd6cc
Michael Willmott <mwillmott@gmail.com>
2018-05-27 21:18:41 +0100

    Remove server files

--- server/auth/handlers.py
+++
@@ -1,5 +0,0 @@
-# Get the email address from the Auth0 payload and
-# return this to identify the user by
-def get_auth0_username_handler(payload):
-    username = payload.get('email')
-    return username
