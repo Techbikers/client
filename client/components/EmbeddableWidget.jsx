1a165855321640f52bcd2d971ad8511e249bd841
Michael Willmott <hi@mwillmott.co>
2019-04-15 22:49:10 +0100

    Introduce an embeddable widget to show the fundraising total for a given ride

---
+++ client/components/EmbeddableWidget.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import SpreadBox from 'techbikers/components/SpreadBox'
+
+const EmbeddableWidget = ({ children }) => (
+  <SpreadBox>
+    {children}
+  </SpreadBox>
+)
+
+export default EmbeddableWidget
